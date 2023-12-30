d842efd6107b7876888280f70e3a17129b6af794
Michael Lilly <michael.lilly@gmail.com>
2023-12-30 10:23:50 -0700

    get tmux.nvim to work by setting options (and forcing require, setup)

--- init.lua
+++ init.lua
@@ -39,7 +39,10 @@ require('lazy').setup({
   'tpope/vim-surround',
 
   -- tmux integration
-  'aserowy/tmux.nvim',
+  {
+    'aserowy/tmux.nvim',
+    opts = {}
+  },
 
   {
     'stevearc/oil.nvim',
@@ -440,7 +443,8 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  -- interferes with tmux keymap
+  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
