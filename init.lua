bfaed6c324eea6d2c89632b6918adc3322bcca1c
Michael Lilly <mplilly@sandia.gov>
2025-01-09 14:30:21 -0700

    whichkey changes suggested by checkhealth

--- init.lua
+++ init.lua
@@ -286,16 +286,26 @@ require('lazy').setup({
 
       -- Document existing key chains
       require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>m'] = { name = 'Harpoon [M]ark', _ = 'which_key_ignore' },
-        ['<leader>n'] = { name = '[N]eorg', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>u'] = { name = '[U]ndotree', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>d_", hidden = true },
+        { "<leader>h", group = "Git [H]unk" },
+        { "<leader>h_", hidden = true },
+        { "<leader>m", group = "Harpoon [M]ark" },
+        { "<leader>m_", hidden = true },
+        { "<leader>n", group = "[N]eorg" },
+        { "<leader>n_", hidden = true },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>r_", hidden = true },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>s_", hidden = true },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>t_", hidden = true },
+        { "<leader>u", group = "[U]ndotree" },
+        { "<leader>u_", hidden = true },
+        { "<leader>w", group = "[W]orkspace" },
+        { "<leader>w_", hidden = true },
       }
       -- visual mode
       require('which-key').register({
